1517290394f25a487b9acdeb6fbae39b040d191c
[jainam2385] <jrs1@somaiya.edu>
2021-02-05 18:49:01 +0530

    Completed user module

--- Backend/QyuApplication/User/urls.py
+++ Backend/QyuApplication/User/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import UserDetailApi
+from .views import (
+    UserDetailApi, 
+    AuthenticateUserApi
+)
 
 
 urlpatterns = [
     path('get-user-detail/', UserDetailApi.as_view(), name="get-user-detail"),
+    path('authenticate-user/', AuthenticateUserApi.as_view(), name="authenticate-user")
 ]
--- Backend/QyuApplication/User/views.py
+++ Backend/QyuApplication/User/views.py
@@ -4,16 +4,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser
 from .models import UserDetail
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import (
+    make_password, 
+    check_password
+    )
 
 def hash_password(password):
     return make_password(password)
 
+
 def confirm_password(password, hashed_password):
     return  check_password(password, hashed_password)
 
+
 def get_user_model(user_id):
     return UserDetail.objects.get(id=user_id)
+
 
 class UserDetailApi(APIView):
     
@@ -40,6 +46,7 @@ class UserDetailApi(APIView):
                 status=status.HTTP_404_NOT_FOUND,
             )
 
+
     def post(self, request):
         user_data = UserDetailSerailizer(data = request.data)
         if user_data.is_valid():
@@ -54,3 +61,58 @@ class UserDetailApi(APIView):
             user_data.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+    def put(self, request):
+        user_id = request.GET["id"]
+        serializer = UserDetailSerailizer(get_user_model(user_id) ,data=request.data)
+        try:
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data, 
+                    status=status.HTTP_201_CREATED
+                )
+
+        except:
+            return Response(
+                serializer.errors, 
+                status=status.HTTP_406_NOT_ACCEPTABLE
+            )
+
+
+    def delete(self, request):
+
+        user_id = request.GET["id"]
+        try:
+            user_model = get_user_model(user_id)
+            user_model.delete()
+            return Response(
+                status=status.HTTP_204_NO_CONTENT
+            )
+
+        except:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class AuthenticateUserApi(APIView):
+
+    permission_classes = [IsAdminUser]
+
+    def post(self, request):
+        try:
+            username_ = request.POST.get("username")
+            password_ = request.POST.get("password")
+            user_model = UserDetail.objects.get(username=username_)
+
+            if confirm_password(password_, user_model.password):
+                return Response({
+                    "authenticate": True,
+                }, status = status.HTTP_200_OK)
+            return Response({
+                "authenticate": False,
+            }, status=status.HTTP_401_UNAUTHORIZED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
