9727e1ff59092fc1d1e7c561124019e59fbba5b5
[jainam2385] <jrs1@somaiya.edu>
2021-02-19 11:19:40 +0530

    Started subscription module

--- Backend/QyuApplication/organization/serializers.py
+++ Backend/QyuApplication/organization/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import OrganizationDetail
 
 class OrganizationDetailSerailizer(serializers.ModelSerializer):
+    
     class Meta:
         model = OrganizationDetail
         fields = "__all__"
