5dea890e13e4d91ed64b3eb4265f11aa02018d47
Jainam Shah <61734413+jainam2385@users.noreply.github.com>
2021-03-15 23:58:32 -0700

    Modifying delete request

--- Backend/QyuApplication/vqueue/views.py
+++ Backend/QyuApplication/vqueue/views.py
@@ -4,6 +4,10 @@ from rest_framework import status
 from rest_framework.permissions import IsAdminUser
 from .models import Queue
 from .serializers import QueueSerializer
+from User.models import UserDetail
+from django.core.mail import send_mail
+from QyuApplication.settings import EMAIL_HOST_USER
+
 
 
 def get_queue_model(queue_id):
@@ -13,6 +17,22 @@ def get_queue_model(queue_id):
 class QueueDetailApi(APIView):
 
     permission_classes = [IsAdminUser]
+
+    def __mail(self, user_id, status):
+        user_model = UserDetail.objects.get(id=user_id)
+        try:
+            if status == 'L':
+                # TODO: left mail template goes here
+                pass
+            elif status == 'R':
+                # TODO: removed mail template goes here
+                pass
+            elif status == 'C':
+                # TODO: complete mail template goes here
+                pass
+            return True
+        except:
+            return False
 
     def get(self, request):
         try:
@@ -70,8 +90,33 @@ class QueueDetailApi(APIView):
             )
 
     def delete(self, request):
-        queue_id = request.GET["queue_id"]
-        user_id = request.GET["user_id"]
-        status = request.GET["status"]
+        try:
+            _event_id = request.GET["queue_id"]
+            _user_id = request.GET["user_id"]
+            _status = request.GET["status"]
+            _status = _status.upper()
 
-        return Response()
+            vqueue_model = Queue.objects.get(user_id = user_id, event_id = _event_id)
+
+            if vqueue_model.status == "W"  and (_status == 'L' or _status == 'R' or _status == 'C'):
+                vqueue_model.status = _status
+                vqueue_model.save()
+                try:
+                    self.__mail(
+                        _user_id,
+                        _status
+                    )
+                except:
+                    return Response(
+                        status = status.HTTP_400_BAD_REQUEST
+                    )
+
+            else:
+                return Response(
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        except:
+            return Response(
+                status = status.HTTP_400_BAD_REQUEST
+            )
